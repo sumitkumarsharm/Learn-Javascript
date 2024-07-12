2d81efd33f2c6b2e4059b04b684081c5c64dbd1f
Sumit0009 <100469632+sumitkumarsharm@users.noreply.github.com>
2024-07-13 00:02:44 +0530

    this is a popup for prictice of javascrips

--- scripts.js
+++ scripts.js
@@ -1,43 +1,16 @@
-const AllCheckBox = document.querySelectorAll(".custom_checkbox");
-const Allinputs = document.querySelectorAll("input");
-const Alert = document.querySelector(".Alert");
-const progressbar = document.querySelector(".progressbar");
-const progresvalue = document.querySelector(".proggress_color");
+const button = document.querySelector(".btn-1");
+const card = document.querySelector(".popup-container");
+const overlay = document.querySelector(".overlay");
 
-let AllGoals = JSON.parse(localStorage.getItem("AllGoals")) || {};
-
-AllCheckBox.forEach((checkbox) => {
-  checkbox.addEventListener("click", (e) => {
-    let Allfield_filled = [...Allinputs].every((input) => {
-      return input.value;
-    });
-    if (Allfield_filled) {
-      checkbox.parentElement.classList.toggle("completed");
-      progresvalue.style.width = "33%";
-      AllGoals
-    } else {
-      progressbar.classList.add("display_Error");
-    }
-  });
+button.addEventListener("click", () => {
+  card.classList.add("popus_open");
 });
 
-Allinputs.forEach((input) => {
-  input.value = AllGoals[input.id].Name;
-  if(AllGoals[input.id].completed){
-    input.parentElement.classList.add("completed");
-  }
-
-
-
-  input.addEventListener("focus", () => {
-    progressbar.classList.remove("display_Error");
-  });
-
-  input.addEventListener("input", (e) => {
-    AllGoals[input.id] = {
-      Name: e.target.value,
-      completed: false,
-    };
-    localStorage.setItem("AllGoals", JSON.stringify(AllGoals));
-  });
+const cross = document.querySelector(".cross");
+cross.addEventListener("click", () => {
+  card.classList.remove("popus_open");
 });
+
+overlay.addEventListener('click',()=>{
+    card.classList.remove("popus_open");
+})
